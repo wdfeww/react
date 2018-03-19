fac195a9be541d062fe7c139ea5e4593b5d8233a
vladimir.uram <vladimir.uram@globallogic.com>
2018-03-19 16:27:55 +0100

    react

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React, {Component} from 'react';
 import CardList from "./CardList";
-import {robots} from './robots';
+// import {robots} from './robots';
 import SearchBox from './SearchBox';
-
+import Scroll from './Scroll';
+import './App.css';
 //STATE >> props
 
 class App extends Component {
@@ -10,23 +11,35 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      robots: robots,
+      robots: [],
       searchfield: ''
     }
   }
 
-  onSearchChange(event){
-
+  componentDidMount(){
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(users => {this.setState({ robots: users })})
   }
 
+  onSearchChange = (event) => {
+    this.setState( {searchfield: event.target.value } );
+  };
+
   render () {
+    const filteredRobots = this.state.robots.filter(robot =>{
+      return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
+    });
     return(
       <div className='tc'>
-        <h1>H1</h1>
-        <SearchBox searchChange={this.onSearchChange()}/>
-        <CardList robots={this.state.robots}/>
+        <h1 className='f1'>Robots</h1>
+        <SearchBox searchChange={this.onSearchChange}/>
+        <Scroll>
+          <CardList robots={filteredRobots}/>
+          {/*cardlist bude teraz children scrollu*/}
+        </Scroll>
       </div>
-    )
+    );
   }
 }
 
